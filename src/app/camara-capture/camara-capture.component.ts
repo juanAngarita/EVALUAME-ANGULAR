b2de5013c0941226d1d0e874ede485cd7b14c88a
juanAngarita <angaritat.js@javeriana.edu.co>
2024-08-07 20:24:08 -0500

    "Actualizando modelo a blazepose"

--- src/app/camara-capture/camara-capture.component.ts
+++ src/app/camara-capture/camara-capture.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import * as poseDetection from '@tensorflow-models/pose-detection';
 import * as tf from '@tensorflow/tfjs';
 import * as backend from '@tensorflow/tfjs-backend-webgpu';
@@ -6,50 +12,86 @@ import { ConfirmationDialogService } from '../services/confirmation-dialog.servi
 import { Escala } from '../model/Escala';
 import { EscalaService } from '../services/escala.service';
 
-
 @Component({
   providers: [ConfirmationDialogService],
   selector: 'app-camara-capture',
   templateUrl: './camara-capture.component.html',
-  styleUrls: ['./camara-capture.component.css']
+  styleUrls: ['./camara-capture.component.css'],
 })
 export class CamaraCaptureComponent implements AfterViewInit {
-
   //DICCIONATIO DE PUNTOS CLAVE
   PUNTOS: { [key: string]: number } = {
     NOSE: 0,
-    LEFT_EYE: 1,
-    RIGHT_EYE: 2,
-    LEFT_EAR: 3,
-    RIGHT_EAR: 4,
-    LEFT_SHOULDER: 5,
-    RIGHT_SHOULDER: 6,
-    LEFT_ELBOW: 7,
-    RIGHT_ELBOW: 8,
-    LEFT_WRIST: 9,
-    RIGHT_WRIST: 10,
-    LEFT_HIP: 11,
-    RIGHT_HIP: 12,
-    LEFT_KNEE: 13,
-    RIGHT_KNEE: 14,
-    LEFT_ANKLE: 15,
-    RIGHT_ANKLE: 16,
-  }
+    LEFT_EYE_INNER: 1,
+    LEFT_EYE: 2,
+    LEFT_EYE_OUTER: 3,
+    RIGHT_EYE_INNER: 4,
+    RIGHT_EYE: 5,
+    RIGHT_EYE_OUTER: 6,
+    LEFT_EAR: 7,
+    RIGHT_EAR: 8,
+    MOUTH_LEFT: 9,
+    MOUTH_RIGHT: 10,
+    LEFT_SHOULDER: 11,
+    RIGHT_SHOULDER: 12,
+    LEFT_ELBOW: 13,
+    RIGHT_ELBOW: 14,
+    LEFT_WRIST: 15,
+    RIGHT_WRIST: 16,
+    LEFT_PINKY: 17,
+    RIGHT_PINKY: 18,
+    LEFT_INDEX: 19,
+    RIGHT_INDEX: 20,
+    LEFT_THUMB: 21,
+    RIGHT_THUMB: 22,
+    LEFT_HIP: 23,
+    RIGHT_HIP: 24,
+    LEFT_KNEE: 25,
+    RIGHT_KNEE: 26,
+    LEFT_ANKLE: 27,
+    RIGHT_ANKLE: 28,
+    LEFT_HEEL: 29,
+    RIGHT_HEEL: 30,
+    LEFT_FOOT_INDEX: 31,
+    RIGHT_FOOT_INDEX: 32,
+  };
 
   //DICCIONARIO DE CONEXIONES
   keypointConnections: { [key: string]: string[] } = {
-    nose: ['left_ear', 'right_ear'],
+    nose: ['left_eye_inner', 'right_eye_inner'],
+    left_eye_inner: ['left_eye', 'left_eye_outer'],
+    right_eye_inner: ['right_eye', 'right_eye_outer'],
+    left_eye: ['left_eye_outer'],
+    right_eye: ['right_eye_outer'],
+    left_eye_outer: ['left_ear'],
+    right_eye_outer: ['right_ear'],
     left_ear: ['left_shoulder'],
     right_ear: ['right_shoulder'],
+    mouth_left: ['mouth_right'],
+    mouth_right: [],
     left_shoulder: ['right_shoulder', 'left_elbow', 'left_hip'],
-    right_shoulder: ['right_elbow', 'right_hip'],
+    right_shoulder: ['left_shoulder', 'right_elbow', 'right_hip'],
     left_elbow: ['left_wrist'],
     right_elbow: ['right_wrist'],
-    left_hip: ['left_knee', 'right_hip'],
-    right_hip: ['right_knee'],
+    left_wrist: ['left_pinky', 'left_index', 'left_thumb'],
+    right_wrist: ['right_pinky', 'right_index', 'right_thumb'],
+    left_pinky: [],
+    right_pinky: [],
+    left_index: [],
+    right_index: [],
+    left_thumb: [],
+    right_thumb: [],
+    left_hip: ['right_hip', 'left_knee'],
+    right_hip: ['left_hip', 'right_knee'],
     left_knee: ['left_ankle'],
-    right_knee: ['right_ankle']
-  }
+    right_knee: ['right_ankle'],
+    left_ankle: ['left_heel', 'left_foot_index'],
+    right_ankle: ['right_heel', 'right_foot_index'],
+    left_heel: ['left_foot_index'],
+    right_heel: ['right_foot_index'],
+    left_foot_index: [],
+    right_foot_index: [],
+  };
 
   //Elementos del HTML
   @ViewChild('video') videoElement!: ElementRef;
@@ -69,17 +111,16 @@ export class CamaraCaptureComponent implements AfterViewInit {
   estado: boolean = false; //SI SE ESTÁ GRABANDO
   poses: any;
   puntaje: number = 3;
-  desactivarBoton: boolean = false
-  colorEsqueleto: string = 'rgb(255, 255, 255)'
-
+  desactivarBoton: boolean = false;
+  colorEsqueleto: string = 'rgb(255, 255, 255)';
 
   //ESTIMACIÓN DE POSE Y MODELOS
-  poseClassifier: any
+  poseClassifier: any;
 
   constructor(
     private confirmationDialogService: ConfirmationDialogService,
     private escalaService: EscalaService
-  ) { }
+  ) {}
 
   //INICIALIZAR LA ESCALA EN BASE AL ID
   ngOnChanges() {
@@ -87,16 +128,18 @@ export class CamaraCaptureComponent implements AfterViewInit {
   }
 
   ngOnInit() {
-    console.log("Funcion init");
+    console.log('Funcion init');
     //!!!CAMBIAR ACÁ EL TIPO DE MODELO!!!
     //1. SINGLEPOSE_LIGHTNING (MoveNet)
     //2. SINGLEPOSE_THUNDER (MoveNet)
-    this.runMovenet()
+    this.runMovenet();
   }
 
   //OBTENER LOS ELEMENTOS DEL HTML + INICIALIZAR LA CAMARA
   ngAfterViewInit() {
     this.video = this.videoElement.nativeElement;
+    this.video!.width = 640;
+    this.video!.height = 480;
     this.canvas = this.canvasElement.nativeElement;
     this.ctx = this.canvas.getContext('2d')!;
     this.initCamera();
@@ -105,25 +148,35 @@ export class CamaraCaptureComponent implements AfterViewInit {
 
   //BOTON DE "INICIAR EVALUACIÓN"
   cambiarEstado(element: any) {
-    let button = element
+    let button = element;
     this.estado = !this.estado;
     if (this.estado == true) {
-      button.textContent = "GRABANDO";
+      button.textContent = 'GRABANDO';
     } else {
       this.openConfirmationDialog();
-      button.textContent = "INICIAR EVALUACIÓN";
-      this.colorEsqueleto = 'rgb(255, 255, 255)'
+      button.textContent = 'INICIAR EVALUACIÓN';
+      this.colorEsqueleto = 'rgb(255, 255, 255)';
     }
   }
 
   //DIALOGO DE CONFIRMACIÓN
   openConfirmationDialog() {
     //EN CASO DE QUE EL USUARIO ACEPTE EL PUNTAJE SE LLAMA A ACTUALIZAR PUNTAJES
-    this.confirmationDialogService.confirm('Confirmar el puntaje', 'El puntaje calculado es: ' + this.puntaje, "Aceptar", "Cancelar")
+    this.confirmationDialogService
+      .confirm(
+        'Confirmar el puntaje',
+        'El puntaje calculado es: ' + this.puntaje,
+        'Aceptar',
+        'Cancelar'
+      )
       .then((confirmed) => this.actualizarPuntajes())
-      .catch(() => console.log('User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)'));
+      .catch(() =>
+        console.log(
+          'User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)'
+        )
+      );
   }
-  //ACTUALIZA EL PUNTAJE TANTO EN EL SERVICIO COMO EN EL FRONT 
+  //ACTUALIZA EL PUNTAJE TANTO EN EL SERVICIO COMO EN EL FRONT
   actualizarPuntajes() {
     if (this.escala) {
       this.escalaService.actualizarPuntaje(this.escala.id, this.puntaje);
@@ -133,34 +186,40 @@ export class CamaraCaptureComponent implements AfterViewInit {
       //EN CASO DE QUE SE HAYA LLEGADO AL MÁXIMO DE INTENTOS
       if (this.escala.intentos == 3) {
         this.desactivarBoton = true;
-        console.log("apagando boton")
+        console.log('apagando boton');
       }
     }
   }
 
   //CARGAR EL MODELO DE CLASIFIFCACION
   async cargarModel() {
+    console.log('Cargando modelo...');
+
+    await tf.ready();
+
     this.poseClassifier = await tf.loadLayersModel('/assets/models/model.json');
-    console.log("Clasificacion de pose :", this.poseClassifier)
-    //console.log(poseClassifier.model)
+    console.log('Clasificacion de pose :', this.poseClassifier);
+    console.log(this.poseClassifier.model);
   }
-
-
 
   async runMovenet() {
     //Esperar a que tf esté listo
     await tf.ready();
     //Crear el detector de pose
-    const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
-    const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+
+    const detector = await poseDetection.createDetector(
+      poseDetection.SupportedModels.BlazePose,
+      { runtime: 'tfjs', modelType: 'heavy' }
+    );
     //obtener las poses
+    //console.log(detector);
     setInterval(() => this.getPoses(detector), 100);
   }
 
   async getPoses(detector: poseDetection.PoseDetector) {
     if (detector && this.video) {
       this.poses = await detector.estimatePoses(this.video);
-      //console.log(this.poses)
+      console.log(this.poses);
       this.drawPoses(this.poses);
     }
   }
@@ -175,12 +234,12 @@ export class CamaraCaptureComponent implements AfterViewInit {
         //SE ALINEA EL TAMAÑO DEL CANVAS Y DEL VIDEO
         this.canvas.width = this.video!.videoWidth;
         this.canvas.height = this.video!.videoHeight;
-        // Llama a la función captureImage dentro del intervalo 
+        // Llama a la función captureImage dentro del intervalo
         //setInterval para actualizar continuamente el contenido del canvas con el marco de video más reciente
         setInterval(() => this.captureImage(), 100);
       };
     } catch (err) {
-      console.error("Error al acceder a la cámara: ", err);
+      console.error('Error al acceder a la cámara: ', err);
     }
   }
 
@@ -192,23 +251,29 @@ export class CamaraCaptureComponent implements AfterViewInit {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       // Dibujar la imagen del video en el canvas
       this.ctx.scale(-1, 1);
-      this.ctx.drawImage(this.video, -this.canvas.width, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(
+        this.video,
+        -this.canvas.width,
+        0,
+        this.canvas.width,
+        this.canvas.height
+      );
       // Restaurar la transformación
       this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
     //RENDERIZADO DE LAS POSES
     if (this.poses) {
-      this.drawPoses(this.poses)
+      this.drawPoses(this.poses);
     }
   }
 
   drawPoses(poses: any) {
     //OBTIENE LOS KEYPOINTS
-    let pose = poses[0].keypoints
+    let pose = poses[0].keypoints;
 
     //Si ya hay poses detectadas
     if (pose) {
-      let fila = []
+      let fila = [];
       //console.log(poses)
       //mostrarResumenDatos();
 
@@ -219,16 +284,17 @@ export class CamaraCaptureComponent implements AfterViewInit {
         let name: string = pose[i].name;
         let score = pose[i].score.toFixed(2);
         if (score > 0.3) {
-          this.pintarPuntosClave(x, y, score, name)
+          this.pintarPuntosClave(x, y, score, name);
           let conexiones: string[] = this.keypointConnections[name];
           if (conexiones) {
             for (let conexion of conexiones) {
-              let conName = conexion.toUpperCase()
+              let conName = conexion.toUpperCase();
               if (pose[this.PUNTOS[conName]].score > 0.3) {
-                this.drawSegment([x, y],
-                  [pose[this.PUNTOS[conName]].x,
-                  pose[this.PUNTOS[conName]].y],
-                  this.colorEsqueleto)
+                this.drawSegment(
+                  [x, y],
+                  [pose[this.PUNTOS[conName]].x, pose[this.PUNTOS[conName]].y],
+                  this.colorEsqueleto
+                );
               }
             }
           }
@@ -237,103 +303,144 @@ export class CamaraCaptureComponent implements AfterViewInit {
         //SI SE ESTÁ GRABANDO SE AGREGAN TODAS LAS POSES AL RREGLO
         if (this.estado) {
           fila.push([parseFloat(x.toFixed(2)), parseFloat(y.toFixed(2))]);
-        } 
-      }
-      
-      if (this.estado) {
-
-        console.log("FILA:" ,fila)
-
-        let predict_data = this.landmarks_to_embedding(fila)
-
-        console.log("PREDICT DATA:" , predict_data)
-
-        let puntaje = this.predecir(predict_data)
-
-        if(puntaje == 0){
-          this.colorEsqueleto = 'rgb(255, 0, 0)'
-        }else if(puntaje == 1){
-          this.colorEsqueleto = 'rgb(255, 255, 0)'
-        }else if(puntaje == 2){
-          this.colorEsqueleto = 'rgb(0, 255, 0)'
         }
       }
 
+      if (this.estado) {
+        //console.log('FILA:', fila);
+
+        let predict_data = this.landmarks_to_embedding(fila);
+
+        // Crear un tensor con el valor adicional que deseas agregar
+        let additional_value = tf.tensor([[this.escala!.id]]); // 'valor' es el valor que deseas agregar
+
+        // Concatenar el nuevo valor al final de predict_data
+        predict_data = tf.concat([predict_data, additional_value], 1);
+
+        console.log('PREDICT DATA:', predict_data);
+
+        let puntaje = this.predecir(predict_data);
+
+        if (puntaje == 0) {
+          this.colorEsqueleto = 'rgb(255, 0, 0)';
+        } else if (puntaje == 1) {
+          this.colorEsqueleto = 'rgb(255, 255, 0)';
+        } else if (puntaje == 2) {
+          this.colorEsqueleto = 'rgb(0, 255, 0)';
+        }
+      }
     }
   }
 
   //PROCESAMIENTO DE DATOS
-  get_center_point(landmarks: any, left_bodypart: number, right_bodypart: number) {
-    let left = tf.gather(landmarks, left_bodypart, 1)
-    let right = tf.gather(landmarks, right_bodypart, 1)
-    const center = tf.add(tf.mul(left, 0.5), tf.mul(right, 0.5))
-    return center
+  get_center_point(
+    landmarks: any,
+    left_bodypart: number,
+    right_bodypart: number
+  ) {
+    let left = tf.gather(landmarks, left_bodypart, 1);
+    let right = tf.gather(landmarks, right_bodypart, 1);
+    const center = tf.add(tf.mul(left, 0.5), tf.mul(right, 0.5));
+    return center;
   }
 
   get_pose_size(landmarks: any, torso_size_multiplier = 2.5) {
-    let hips_center = this.get_center_point(landmarks, this.PUNTOS["LEFT_HIP"], this.PUNTOS["RIGHT_HIP"])
-    let shoulders_center = this.get_center_point(landmarks, this.PUNTOS["LEFT_SHOULDER"], this.PUNTOS["RIGHT_SHOULDER"])
-    let torso_size = tf.norm(tf.sub(shoulders_center, hips_center))
-    let pose_center_new = this.get_center_point(landmarks, this.PUNTOS["LEFT_HIP"], this.PUNTOS["RIGHT_HIP"])
-    pose_center_new = tf.expandDims(pose_center_new, 1)
+    let hips_center = this.get_center_point(
+      landmarks,
+      this.PUNTOS['LEFT_HIP'],
+      this.PUNTOS['RIGHT_HIP']
+    );
+    let shoulders_center = this.get_center_point(
+      landmarks,
+      this.PUNTOS['LEFT_SHOULDER'],
+      this.PUNTOS['RIGHT_SHOULDER']
+    );
+    let torso_size = tf.norm(tf.sub(shoulders_center, hips_center));
+    let pose_center_new = this.get_center_point(
+      landmarks,
+      this.PUNTOS['LEFT_HIP'],
+      this.PUNTOS['RIGHT_HIP']
+    );
+    pose_center_new = tf.expandDims(pose_center_new, 1);
 
-    pose_center_new = tf.broadcastTo(pose_center_new,
-      [1, 17, 2]
-    )
+    pose_center_new = tf.broadcastTo(pose_center_new, [1, 33, 2]);
     // return: shape(17,2)
-    let d = tf.gather(tf.sub(landmarks, pose_center_new), 0, 0)
-    let max_dist = tf.max(tf.norm(d, 'euclidean', 0))
+    let d = tf.gather(tf.sub(landmarks, pose_center_new), 0, 0);
+    let max_dist = tf.max(tf.norm(d, 'euclidean', 0));
 
     // normalize scale
-    let pose_size = tf.maximum(tf.mul(torso_size, torso_size_multiplier), max_dist)
-    return pose_size
+    let pose_size = tf.maximum(
+      tf.mul(torso_size, torso_size_multiplier),
+      max_dist
+    );
+    return pose_size;
   }
 
   normalize_pose_landmarks(landmarks: any) {
+    let pose_center = this.get_center_point(
+      landmarks,
+      this.PUNTOS['LEFT_HIP'],
+      this.PUNTOS['RIGHT_HIP']
+    );
 
-    let pose_center = this.get_center_point(landmarks, this.PUNTOS["LEFT_HIP"], this.PUNTOS["RIGHT_HIP"])
+    pose_center = tf.expandDims(pose_center, 1);
 
-    pose_center = tf.expandDims(pose_center, 1)
+    pose_center = tf.broadcastTo(pose_center, [1, 33, 2]);
 
-    pose_center = tf.broadcastTo(pose_center,
-      [1, 17, 2]
-    )
+    landmarks = tf.sub(landmarks, pose_center);
 
-    landmarks = tf.sub(landmarks, pose_center)
-
-    let pose_size = this.get_pose_size(landmarks)
-    landmarks = tf.div(landmarks, pose_size)
+    let pose_size = this.get_pose_size(landmarks);
+    landmarks = tf.div(landmarks, pose_size);
 
     landmarks.array().then((array: any[]) => {
-      console.log("Nuevo:", array);
+      console.log('Nuevo:', array);
     });
-    return landmarks
+    return landmarks;
   }
 
   landmarks_to_embedding(landmarks: any) {
     // normalize landmarks 2D
-    landmarks = this.normalize_pose_landmarks(tf.expandDims(landmarks, 0))
-    let embedding = tf.reshape(landmarks, [1, 34])
-    return embedding
+    landmarks = this.normalize_pose_landmarks(tf.expandDims(landmarks, 0));
+    let embedding = tf.reshape(landmarks, [1, 66]);
+    return embedding;
   }
   //////////
   //FIN PROCESAMIENTO DE DATOS
 
-  predecir(processedInput: any){
-    const classification = this.poseClassifier.predict(processedInput)
-  
+  predecir(processedInput: any) {
+    const classification = this.poseClassifier.predict(processedInput);
+
     // Get the value
-    const value = classification.argMax(-1).dataSync()[0]
-  
-        // Arreglo proporcionado
-    var arreglo = ['110', '111', '112', '150', '151', '152', '160', '161', '162', '170', '171', '172', '210', '211', '212', '300', '301', '302'];
+    const value = classification.argMax(-1).dataSync()[0];
+
+    // Arreglo proporcionado
+    var arreglo = [
+      '110',
+      '111',
+      '112',
+      '150',
+      '151',
+      '152',
+      '160',
+      '161',
+      '162',
+      '170',
+      '171',
+      '172',
+      '210',
+      '211',
+      '212',
+      '300',
+      '301',
+      '302',
+    ];
 
     // Crear un diccionario donde la clave sea una secuencia de números que comience en 0
-    var diccionario: {[key: string]: string} = {};
+    var diccionario: { [key: string]: string } = {};
 
     // Llenar el diccionario
     for (var i = 0; i < arreglo.length; i++) {
-        diccionario[i.toString()] = arreglo[i];
+      diccionario[i.toString()] = arreglo[i];
     }
 
     // Convertir el número en una cadena
@@ -345,8 +452,8 @@ export class CamaraCaptureComponent implements AfterViewInit {
     // Obtener el último dígito
     var ultimoDigito = parseInt(numeroStr.substring(2));
 
-    console.log("PRUEBA", dosPrimerosDigitos)
-    console.log("PREDICCION", ultimoDigito)
+    console.log('PRUEBA', dosPrimerosDigitos);
+    console.log('PREDICCION', ultimoDigito);
     return ultimoDigito;
   }
 
@@ -367,15 +474,19 @@ export class CamaraCaptureComponent implements AfterViewInit {
   }
 
   //DIBUJAR SEGMENTO DE LINEA
-  drawSegment([mx, my]: [number, number], [tx, ty]: [number, number], color: string) {
+  drawSegment(
+    [mx, my]: [number, number],
+    [tx, ty]: [number, number],
+    color: string
+  ) {
     let mirroredmX = this.canvas.width - mx;
     let mirroredtX = this.canvas.width - tx;
 
-    this.ctx.beginPath()
-    this.ctx.moveTo(mirroredmX, my)
-    this.ctx.lineTo(mirroredtX, ty)
-    this.ctx.lineWidth = 5
-    this.ctx.strokeStyle = color
-    this.ctx.stroke()
+    this.ctx.beginPath();
+    this.ctx.moveTo(mirroredmX, my);
+    this.ctx.lineTo(mirroredtX, ty);
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = color;
+    this.ctx.stroke();
   }
 }
--- src/app/services/escala.service.ts
+++ src/app/services/escala.service.ts
@@ -2,94 +2,110 @@ import { Injectable } from '@angular/core';
 import { Escala } from '../model/Escala';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EscalaService {
-
-  constructor() { }
+  constructor() {}
 
   escalas: Escala[] = [
     {
-      "id":1,
-      "posicionInicial": "Sentado en borde de camilla o silla (pies sin apoyar) o en camilla/suelo (piesapoyados). Sin silla de ruedas. Sin apoyar la espalda.",
-      "descripcion": "1.UNA MANO A LA CABEZA SENTADO",
-      "intentos": 0,
-      "puntajeMaximo":0,
-      "detalles":{
-        "puntaje0":"No puede traer las manos a la cabeza, siquiera usando el movimiento de la cabeza o el tronco",
-        "puntaje1":"Solo puede traer la mano a la cabeza al flexionar la cabeza",
-        "puntaje2":"Puede traer un mano a la cabeza. Cabeza y tronco estables",
-        "instrucciones":"¿Puedes sentarte en la camilla/silla sin apoyar las manos durante 3 segundos?"
+      id: 3,
+      posicionInicial:
+        'Sentado en borde de camilla o silla (pies sin apoyar) o en camilla/suelo (piesapoyados). Sin silla de ruedas. Sin apoyar la espalda.',
+      descripcion: '1.UNA MANO A LA CABEZA SENTADO',
+      intentos: 0,
+      puntajeMaximo: 0,
+      detalles: {
+        puntaje0:
+          'No puede traer las manos a la cabeza, siquiera usando el movimiento de la cabeza o el tronco',
+        puntaje1: 'Solo puede traer la mano a la cabeza al flexionar la cabeza',
+        puntaje2: 'Puede traer un mano a la cabeza. Cabeza y tronco estables',
+        instrucciones:
+          '¿Puedes sentarte en la camilla/silla sin apoyar las manos durante 3 segundos?',
       },
-      "puntajes": [0,0,0]
+      puntajes: [0, 0, 0],
     },
     {
-      "id":2,
-      "posicionInicial": "En decúbito prono, brazos a los lados (ver ítem 6) (pelvis en contacto con elsuelo).",
-      "descripcion": "2.Se apoya sobre los brazos extendidos",
-      "intentos": 0,
-      "puntajeMaximo":0,
-      "detalles":{
-        "puntaje0":"Incapaz",
-        "puntaje1":"Puede apoyarsesobre los brazosextendidos durante3’’ si se le coloca.",
-        "puntaje2":"Capaz de apoyarsesobre los brazosextendidos, con lacabeza en altodurante 3",
-        "instrucciones":"¿puedes llevar una mano a tu cabeza sin flexionar tu cuello?"
+      id: 4,
+      posicionInicial:
+        'En decúbito prono, brazos a los lados (ver ítem 6) (pelvis en contacto con elsuelo).',
+      descripcion: '2.Se apoya sobre los brazos extendidos',
+      intentos: 0,
+      puntajeMaximo: 0,
+      detalles: {
+        puntaje0: 'Incapaz',
+        puntaje1:
+          'Puede apoyarsesobre los brazosextendidos durante3’’ si se le coloca.',
+        puntaje2:
+          'Capaz de apoyarsesobre los brazosextendidos, con lacabeza en altodurante 3',
+        instrucciones:
+          '¿puedes llevar una mano a tu cabeza sin flexionar tu cuello?',
       },
-      "puntajes": [0,0,0]
+      puntajes: [0, 0, 0],
     },
     {
-      "id":3,
-      "posicionInicial": "En decúbito prono, brazos a los lados (ver ítem 6). Frente apoyada en elsuelo.",
-      "descripcion": "Ítem 12:Levanta la cabeza desde decúbito prono",
-      "intentos": 0,
-      "puntajeMaximo":0,
-      "detalles":{
-        "puntaje0":"Incapaz.",
-        "puntaje1":"Levanta la cabezacon los brazos haciael frente durante 3’’. ",
-        "puntaje2":"Capaz de levantar lacabeza en decúbitoprono con los brazosa los lados durante3’’.",
-        "instrucciones":"¿Puedes levantar la cabeza manteniendo los brazos a los lados durante 3 segundos?"
+      id: 5,
+      posicionInicial:
+        'En decúbito prono, brazos a los lados (ver ítem 6). Frente apoyada en elsuelo.',
+      descripcion: 'Ítem 12:Levanta la cabeza desde decúbito prono',
+      intentos: 0,
+      puntajeMaximo: 0,
+      detalles: {
+        puntaje0: 'Incapaz.',
+        puntaje1:
+          'Levanta la cabezacon los brazos haciael frente durante 3’’. ',
+        puntaje2:
+          'Capaz de levantar lacabeza en decúbitoprono con los brazosa los lados durante3’’.',
+        instrucciones:
+          '¿Puedes levantar la cabeza manteniendo los brazos a los lados durante 3 segundos?',
       },
-      "puntajes": [0,0,0]
+      puntajes: [0, 0, 0],
     },
     {
-      "id":4,
-      "posicionInicial": "En supino, con caderas y rodillas en extensión máxima posible.",
-      "descripcion": "Ítem 21:Flexión de cadera derecha en supino",
-      "intentos": 0,
-      "puntajeMaximo":0,
-      "detalles":{
-        "puntaje0":"Incapaz.",
-        "puntaje1":"Inicia flexión decadera y rodilladerechas(rango articulardisponible superior a10º*). ",
-        "puntaje2":"Logra flexióncompleta de caderaderecha.",
-        "instrucciones":"Puedes llevar la rodilla derecha al pecho? Los individuos deberían realizar esta actividad sin apoyo de las manos"
+      id: 6,
+      posicionInicial:
+        'En supino, con caderas y rodillas en extensión máxima posible.',
+      descripcion: 'Ítem 21:Flexión de cadera derecha en supino',
+      intentos: 0,
+      puntajeMaximo: 0,
+      detalles: {
+        puntaje0: 'Incapaz.',
+        puntaje1:
+          'Inicia flexión decadera y rodilladerechas(rango articulardisponible superior a10º*). ',
+        puntaje2: 'Logra flexióncompleta de caderaderecha.',
+        instrucciones:
+          'Puedes llevar la rodilla derecha al pecho? Los individuos deberían realizar esta actividad sin apoyo de las manos',
       },
-      "puntajes": [0,0,0]
+      puntajes: [0, 0, 0],
     },
     {
-      "id":5,
-      "posicionInicial": "En decúbito prono, brazos en posición media o a los lados",
-      "descripcion": "Ítem 15: Cuadrupedia",
-      "intentos": 0,
-      "puntajeMaximo":0,
-      "detalles":{
-        "puntaje0":"Incapaz.",
-        "puntaje1":"Mantiene la posicióndurante 3’’ cuandose le coloca.",
-        "puntaje2":"Consigue ponerse enposición decuadrupedia con lacabeza en altodurante 3’’.",
-        "instrucciones":"¿Puedes ponerte sobre las manos y las rodillas, con la cabeza en alto ymantener la posición durante 3 segundos?"
+      id: 7,
+      posicionInicial:
+        'En decúbito prono, brazos en posición media o a los lados',
+      descripcion: 'Ítem 15: Cuadrupedia',
+      intentos: 0,
+      puntajeMaximo: 0,
+      detalles: {
+        puntaje0: 'Incapaz.',
+        puntaje1: 'Mantiene la posicióndurante 3’’ cuandose le coloca.',
+        puntaje2:
+          'Consigue ponerse enposición decuadrupedia con lacabeza en altodurante 3’’.',
+        instrucciones:
+          '¿Puedes ponerte sobre las manos y las rodillas, con la cabeza en alto ymantener la posición durante 3 segundos?',
       },
-      "puntajes": [0,0,0]
+      puntajes: [0, 0, 0],
     },
-  ]
+  ];
 
-  findAll(){
+  findAll() {
     return this.escalas;
   }
 
-  findById(id:number){
-    return this.escalas[id-1];
+  findById(id: number) {
+    return this.escalas[id - 1];
   }
 
-  actualizarPuntaje(idEscala:number, puntaje: number){
+  actualizarPuntaje(idEscala: number, puntaje: number) {
     let intentos = this.escalas[idEscala].intentos;
     this.escalas[idEscala].puntajes[intentos] = puntaje;
     this.escalas[idEscala].intentos++;
